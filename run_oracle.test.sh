856522e4e64efa4046a325e7522b0d093fce6964
bhaskard <bhaskard@stanford.edu>
2018-06-17 16:45:03 +0000

    converting .travis.yml to cloudbuild.yaml

---
+++ run_oracle.test.sh
@@ -0,0 +1,1 @@
+mvn -e -Djava.security.egd=file:/dev/./urandom -Ddatabase.url=jdbc:oracle:thin:@$ORACLEDB_SERVER:1521/ORCLCDB.localdomain -Ddatabase.user='sys as sysdba' -Ddatabase.password=Oradoc_db1 -P oracle12 test
